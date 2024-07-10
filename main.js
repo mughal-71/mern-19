0d30af469deae318c4928924213c89cf1a07bf94
mughal-71 <abdullahmughal71.com@gmail.com>
2024-07-10 13:49:55 +0500

    added a new function

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
+const primary = () =>{
+    console.log("primary");
+};
+
 const main = () => {
     console.log("main");
 };
 
-const primary = () =>{
-    console.log("primary");
-};
 
 const secondry = () =>{
     console.log("secondry");
